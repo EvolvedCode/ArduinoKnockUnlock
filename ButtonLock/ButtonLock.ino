986eff232a4cb95dba692c1b133ce9a57cb0ef8f
EvolvedCode <nickstathas@outlook.com>
2015-11-29 18:44:11 +0200

    Quick Changes in ButtonLock
    Reordered the global variable setup, so that user defined variable
    definitions are easy to locate. Reordered some code. Updated and added
    comments.

--- ButtonLock/ButtonLock.ino
+++ ButtonLock/ButtonLock.ino
@@ -1,24 +1,26 @@
 //Pin Cofiguration
-int pButton = 4; //pattern button
-int rButton = 6; //record button
-int buzzer = 9; //buzzer or speaker
-int led = 13; //led
+const int pButton = 4; //pattern button
+const int rButton = 6; //record button
+const int buzzer = 9; //buzzer or speaker
+const int led = 13; //led
 
+//General Configuration
+const int maxpush = 10;  //Set this to the maximum amount of pushes you should expect.
+const int longest = 3000; //Set this to the maximum amount of time you should expect between current and possibly recorded pushes. Have it be longer than your longest push.
+int push_cnt = 7;  //Set this to the amount of knocks in your default pattern. //This is used to prevent code from going out of bounds and into useless data on the arrays.
+int ref_delta[maxpush]={0, 161, 100, 96, 231, 503, 171}, ref_duration[maxpush]={115, 86, 81, 86, 130, 110, 136};//Set a default pattern here.
+int res = 5; //The higher this is, the more inaccurate the measuring of pushes will be.
+int tolerance = 200; //How many milliseconds the user can be late.
+
+//Variable Setup
 bool counting = false;
 bool rcounting = false;
 bool recording = false;
 bool recorded = false; //True if the user has recorded at least one full push.
-const int maxpush = 10;  //Set this to the maximum amount of pushes you should expect.
-int ref_delta[maxpush]={0, 161, 100, 96, 231, 503, 171}, ref_duration[maxpush]={115, 86, 81, 86, 130, 110, 136};//Set a default pattern here.
-int push_cnt = 7;  //Set this to the amount of knocks in your default pattern.
 int delta[maxpush], duration[maxpush];
 int current = 0;
 int rcurrent = 0;
-long start, finish;
-int longest = 3000;
-int res = 5; //The higher this is, the more inaccurate the measuring of pushes will be.
-int tolerance = 200; //How many milliseconds the user can be late.
-int valid;
+unsigned long start, finish;
 
 void setup(){
   Serial.begin(9600);
@@ -48,18 +50,18 @@ void setup(){
   else digitalWrite(led, LOW);
 }
 
-void unlock(){
+void unlock(){ //Feel free to write an unlock function that suits your resources and needs. This gets executed only if the correct pattern is input at record time.
   digitalWrite(buzzer, HIGH);
   delay(250);
   digitalWrite(buzzer, LOW);
   Serial.println("OPEN");
 }
 
-void check(){
+void check(){ //The function responsible for checking whether or not the pattern input at record time is satisfactory, and for issuing the unlock command.
   Serial.println("checking!");
   delta[0] = 0;
   ref_delta[0] = 0;
-  valid = 0;
+  int valid = 0;
   current = 0;
   for(int i = 0; i < push_cnt; i++){
     if(abs(ref_delta[i] - delta[i]) < tolerance) valid++;
@@ -91,9 +93,9 @@ void loop(){
   
       if(digitalRead(pButton) == 1 && rcounting){
         ref_delta[rcurrent] = start - finish; //This finish is the end of the previous push. Essentially we're finding out how far apart the current and the previous push were.
-        Serial.println(ref_delta[rcurrent]);
         finish = millis();
-        ref_duration[rcurrent] = finish - start; //This finds the Î”t or, in other words, the duration of the current push.
+        ref_duration[rcurrent] = finish - start; //This finds the Δt or, in other words, the duration of the current push.
+        Serial.println(ref_delta[rcurrent]);
         Serial.println(ref_duration[rcurrent]);
         rcurrent++;
         push_cnt = rcurrent;
@@ -117,11 +119,11 @@ void loop(){
       if(digitalRead(pButton) == 1 && counting){
         delta[current] = start - finish; //This finish is the end of the previous push. Essentially we're finding out how far apart the current and the previous push were.
         finish = millis();
-        duration[current] = finish - start; //This finds the Î”t or in other words the duration of the current push.
+        duration[current] = finish - start; //This finds the Δt or in other words the duration of the current push.
         Serial.println(String("delta ")+String(delta[current]));
+        Serial.println(String("dur ")+String(finish-start));
         current++;
         counting = false;
-        Serial.println(String("dur ")+String(finish-start));
       }
     }
     else check();
